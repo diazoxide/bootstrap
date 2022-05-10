f31e141fe7576bf94ee5e0b5ac07915a7f984d36
Aaron <aharon@easydmarc.us>
2022-05-10 23:12:17 +0400

    Initial

--- bs.py
+++ bs.py
@@ -9,7 +9,7 @@ class Bootstrap:
     name: str
     root_directory: str = '~/bs-project'
     modules: list = []
-    env: str = 'dev'
+    default_env: str = 'dev'
     external_modules: list = []
     variables: dict = {}
 
@@ -48,59 +48,63 @@ class Bootstrap:
         module.bootstrap = self
         return self
 
-    def get_module_root_dir(self, module: Module | str):
+    def get_module_root_dir(self, module: Module | str) -> str:
         module = self.get_module(module)
-        return self.root_directory + '/' + self.env + '/' + (module.root_directory_name or module.name)
+        return self.root_directory + '/' + self.default_env + '/' + (module.root_directory_name or module.name)
 
-    def get_module_dir(self, module: Module | str, external: bool = False):
+    def get_module_dir(self, module: Module | str, external: bool = False) -> str:
         module_name = module.name if isinstance(module, Bootstrap.Module) else module
         if external or isinstance(module, Bootstrap.Module) and module.external:
             return self.__external_modules_directory + '/' + module_name
         return self.__modules_directory + '/' + module_name
 
-    def get_module_env(self, module: Module | str):
+    def get_module_env_variables(self, module: Module | str, env: str) -> dict:
         module = self.get_module(module)
 
-        env = os.environ.copy()
-        env['root_dir'] = self.get_module_root_dir(module)
+        variables = os.environ.copy()
+        variables['BS_ROOT_DIR'] = self.get_module_root_dir(module)
+        variables['BS_ENV'] = env
+        for _module in self.modules:
+            variables['BS_' + _module.name.upper().replace('-', '_') + "_SERVICE"] = self.get_service_name(_module, env)
+
         try:
-            bootstrap_variables = self.variables[self.env]
+            bootstrap_variables = self.variables[env]
         except KeyError:
             bootstrap_variables = {}
 
         try:
-            module_variables = module.variables[self.env]
+            module_variables = module.variables[env]
         except KeyError:
             module_variables = {}
 
-        env.update(bootstrap_variables)
-        env.update(module_variables)
+        variables.update(bootstrap_variables)
+        variables.update(module_variables)
 
-        return env
+        return variables
 
-    def get_stack_name(self, module: Module):
-        return '{0}-{1}-{2}'.format(self.name, self.env, module.name)
+    def get_stack_name(self, module: Module, env: str) -> str:
+        return '{0}-{1}-{2}'.format(self.name, env, module.name)
 
-    def up_module(self, module: Module | str, rebuild: bool, remote: bool):
-        print(self.get_module_dir(module))
+    def up_module(self, module: Module | str, rebuild: bool = False, remote: bool = False, env: str | None = None):
+        module = self.get_module(module)
+        env = env or self.default_env
         os.chdir(self.get_module_dir(module))
         command = [
             'docker-compose',
             '-p',
-            self.get_stack_name(module),
-            # '--env-file=./env/' + self.env + '.env',
+            self.get_stack_name(module, env),
             'up', '-d', '--force-recreate'
         ]
 
         if rebuild:
             command.append('--build')
 
-        res = subprocess.run(command, env=self.get_module_env(module))
+        res = subprocess.run(command, env=self.get_module_env_variables(module, env))
 
         if res.returncode == 0:
-            self.exec_module_commands(module, 'up', remote)
+            self.exec_module_commands(module, 'up', remote, env)
 
-    def get_module(self, module: str | Module):
+    def get_module(self, module: str | Module) -> Module:
         if isinstance(module, Bootstrap.Module):
             return module
 
@@ -110,14 +114,14 @@ class Bootstrap:
 
         raise Exception('Module ' + module + ' not found.')
 
-    def exec_module_commands(self, module: Module | str, on: str, remote: bool):
+    def exec_module_commands(self, module: Module | str, on: str, remote: bool, env: str):
         module = self.get_module(module)
 
         for command in module.commands:
             if command.on == on:
-                self.exec_module_command(module, command, remote)
+                self.exec_module_command(module, command, remote, env)
 
-    def exec_module_command(self, module: Module | str, command: Module.Command, remote: bool):
+    def exec_module_command(self, module: Module | str, command: Module.Command, remote: bool, env: str):
 
         module = self.get_module(module)
 
@@ -136,44 +140,66 @@ class Bootstrap:
                     must_exec = True
 
             if must_exec:
-                self.exec(command.module or module, command.container, single_command)
+                self.exec(command.module or module, command.container, single_command, env)
 
-        self.exec_module_commands(module, 'after-command-exec', remote)
+        self.exec_module_commands(module, 'after-command-exec', remote, env)
 
-    def up(self, rebuild: bool | str = False, remote: bool | str = False):
-        rebuild = True if rebuild == 'true' or rebuild == True else False
-        remote = True if remote == 'true' or remote == True else False
+    def up(self, rebuild: bool | str = False, env: str | None = None, remote: bool | str = False):
+        rebuild = True if rebuild == 'true' or rebuild else False
+        remote = True if remote == 'true' or remote else False
+        env = env or self.default_env
         for module in self.modules:
-            self.up_module(module, rebuild, remote)
+            self.up_module(module, rebuild, remote, env)
 
-    def get_container_name(self, module: Module, container: str):
-        return self.name + '-' + self.env + '-' + module.name + '-' + container + '-1'
+    def get_service_name(self, module: Module, env: str) -> str:
+        return self.name + '-' + env + '-' + module.name
 
-    def exec(self, module: Module | str, container: str, command: str):
+    def get_container_name(self, module: Module, container: str, env: str) -> str:
+        return self.get_service_name(module, env) + '-' + container + '-1'
 
+    def exec(self, module: Module | str, container: str, command: str, env: str | None = None):
+        env = env or self.default_env
         module = self.get_module(module)
         os.chdir(self.get_module_dir(module))
+        variables = self.get_module_env_variables(module, env)
 
-        variables = self.get_module_env(module)
-        _command_str = ('docker compose -p' + self.get_stack_name(module) + ' exec ' + container + ' ' + command) \
-            .format(**self.get_module_env(module))
+        _command_str = ('docker compose -p' + self.get_stack_name(module, env) + ' exec ' + container + ' ' + command) \
+            .format(**variables)
         _command = shlex.split(_command_str)
 
         return subprocess.run(_command, env=variables)
 
+    @staticmethod
+    def init_from_json(json_name: str = 'bs.json'):
+        with open('./' + json_name, 'r') as json_file:
+            data = json_file.read()
+        _bs = jsonpickle.decode(data)
+        if isinstance(_bs, Bootstrap):
+            _bs.prepare()
+            return _bs
 
-# bs = Bootstrap('xcontain','local', modules=[
-#     Bootstrap.Module('monolith'),
-# ])
-# json_data = jsonpickle.encode(bs)
-# print(json_data)
-# exit(0)
+        raise Exception('Invalid Bootstrap json file')
 
-with open('./bs.json', 'r') as file:
-    json_data = file.read()
+    @staticmethod
+    def setup():
+        if os.path.isfile('./bs.json'):
+            raise Exception('Bootstrap already inited.')
+        _bs = Bootstrap()
+        _bs.name = input("Name of bootstrap: ")
+        _bs.modules = []
+        _bs.external_modules = []
 
-bs = jsonpickle.decode(json_data)
-bs.prepare()
+        json_data = jsonpickle.encode(_bs, indent=4)
+
+        f = open("./bs.json", "w")
+        f.write(json_data)
+        f.close()
+
+try:
+    bs = Bootstrap.init_from_json()
+except Exception:
+    bs = Bootstrap
+
 method = sys.argv[1]
 args_obj = {}
 
