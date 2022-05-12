ea10d13eb45c081f8c6924978b00e787ce8d8983
Aaron Yordanyan <aaron.yor@gmail.com>
2022-05-13 01:12:47 +0400

    Installation fix

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 INSTALL_DIR=/usr/local/lib/bs
-
-mkdir -p $INSTALL_DIR \
-&& git clone https://github.com/diazoxide/bootstrap.git $INSTALL_DIR \
-&& sudo chmod +x "${INSTALL_DIR}/bs.sh" \
-&& sudo rm /usr/local/bin/bs \
-&& sudo ln -s "${INSTALL_DIR}/bs.sh" "" \
-&& echo  -e "\033[92m --- Successfully done. Run 'bs help' to start. --- \033[0m"
+mkdir -p $INSTALL_DIR
+cd $INSTALL_DIR
+git pull || git clone https://github.com/diazoxide/bootstrap.git . \
+&& chmod +x "${INSTALL_DIR}/bs.sh" \
+&& ln -s "${INSTALL_DIR}/bs.sh" "" \
+|| echo -e "\033[92m --- Successfully done. Run 'bs help' to start. --- \033[0m"
