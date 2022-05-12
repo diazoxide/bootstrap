e12f00a1bc43c843aecaa3380dd5e65bb888521c
Aaron Yordanyan <aaron.yor@gmail.com>
2022-05-13 01:32:01 +0400

    Installation fix

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ mkdir -p $INSTALL_DIR
 cd $INSTALL_DIR
 
 git clone https://github.com/diazoxide/bootstrap.git . \
-&& pip3 install -r requirements.txt
+&& pip3 install -r requirements.txt \
 && chmod +x "${INSTALL_DIR}/bs.sh" \
 && ln -s "${INSTALL_DIR}/bs.sh" /usr/local/bin/bs \
 || echo "\033[92m --- Successfully done. Run 'bs help' to start. --- \033[0m"
