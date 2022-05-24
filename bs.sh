1fce032b8854465cef4608e6ffd2e81cec251a1e
Aaron Yordanyan <aaron.yor@gmail.com>
2022-05-24 13:17:41 +0400

    Minor fixes

--- bs.sh
+++ bs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 #TODO change back
-#python3 ~/PycharmProjects/bootstrap/bs.py "$@"
-python3 /usr/local/lib/bs/bs.py "$@"
+python3 ~/PycharmProjects/bootstrap/bs.py "$@"
+#python3 /usr/local/lib/bs/bs.py "$@"
