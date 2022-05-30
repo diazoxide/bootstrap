07b464e9ca104abb3db7ada42245502d6ddef567
Aaron Yordanyan <aaron.yor@gmail.com>
2022-05-30 09:04:39 +0400

    To yaml

--- bs.sh
+++ bs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-python3 ~/PycharmProjects/bootstrap/bs.py "$@"
-#python3 /usr/local/lib/bs/bs.py "$@"
+#python3 ~/PycharmProjects/bootstrap/bs.py "$@"
+python3 /usr/local/lib/bs/bs.py "$@"
